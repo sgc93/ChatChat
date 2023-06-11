220446f9e4d157cf89d957e683970d2cd7ed9f7d
sgc93 <smachewgedefaw@gmail.com>
2023-06-10 17:28:27 -0900

    implementation for password visiblility

---
+++ src/main/java/com/chat/utilities/Password.java
@@ -0,0 +1,43 @@
+package com.chat.utilities;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import javafx.scene.control.PasswordField;
+import javafx.scene.shape.SVGPath;
+
+public class Password {
+    private static boolean isPasswordVisible = false;
+
+    public Password(){
+
+    }
+
+    public static void showPassword(PasswordField pass_field, SVGPath eye){
+        String openedEye = "M6.30147 15.5771C4.77832 14.2684 3.6904 12.7726 3.18002 12C3.6904 11.2274 4.77832 9.73158 6.30147 8.42294C7.87402 7.07185 9.81574 6 12 6C14.1843 6 16.1261 7.07185 17.6986 8.42294C19.2218 9.73158 20.3097 11.2274 20.8201 12C20.3097 12.7726 19.2218 14.2684 17.6986 15.5771C16.1261 16.9282 14.1843 18 12 18C9.81574 18 7.87402 16.9282 6.30147 15.5771ZM12 4C9.14754 4 6.75717 5.39462 4.99812 6.90595C3.23268 8.42276 2.00757 10.1376 1.46387 10.9698C1.05306 11.5985 1.05306 12.4015 1.46387 13.0302C2.00757 13.8624 3.23268 15.5772 4.99812 17.0941C6.75717 18.6054 9.14754 20 12 20C14.8525 20 17.2429 18.6054 19.002 17.0941C20.7674 15.5772 21.9925 13.8624 22.5362 13.0302C22.947 12.4015 22.947 11.5985 22.5362 10.9698C21.9925 10.1376 20.7674 8.42276 19.002 6.90595C17.2429 5.39462 14.8525 4 12 4ZM10 12C10 10.8954 10.8955 10 12 10C13.1046 10 14 10.8954 14 12C14 13.1046 13.1046 14 12 14C10.8955 14 10 13.1046 10 12ZM12 8C9.7909 8 8.00004 9.79086 8.00004 12C8.00004 14.2091 9.7909 16 12 16C14.2092 16 16 14.2091 16 12C16 9.79086 14.2092 8 12 8Z";
+        String crossedEye = "M12 6c3.79 0 7.17 2.13 8.82 5.5-.59 1.22-1.42 2.27-2.41 3.12l1.41 1.41c1.39-1.23 2.49-2.77 3.18-4.53C21.27 7.11 17 4 12 4c-1.27 0-2.49.2-3.64.57l1.65 1.65C10.66 6.09 11.32 6 12 6zm-1.07 1.14L13 9.21c.57.25 1.03.71 1.28 1.28l2.07 2.07c.08-.34.14-.7.14-1.07C16.5 9.01 14.48 7 12 7c-.37 0-.72.05-1.07.14zM2.01 3.87l2.68 2.68C3.06 7.83 1.77 9.53 1 11.5 2.73 15.89 7 19 12 19c1.52 0 2.98-.29 4.32-.82l3.42 3.42 1.41-1.41L3.42 2.45 2.01 3.87zm7.5 7.5l2.61 2.61c-.04.01-.08.02-.12.02-1.38 0-2.5-1.12-2.5-2.5 0-.05.01-.08.01-.13zm-3.4-3.4l1.75 1.75c-.23.55-.36 1.15-.36 1.78 0 2.48 2.02 4.5 4.5 4.5.63 0 1.23-.13 1.77-.36l.98.98c-.88.24-1.8.38-2.75.38-3.79 0-7.17-2.13-8.82-5.5.7-1.43 1.72-2.61 2.93-3.53z";
+        
+        isPasswordVisible = !isPasswordVisible;
+        if (isPasswordVisible) {
+            eye.setContent(crossedEye);
+            pass_field.setPromptText(pass_field.getText());
+            pass_field.clear();
+            pass_field.setDisable(isPasswordVisible);
+            pass_field.setOpacity(1);
+        } else {
+            eye.setContent(openedEye);
+            pass_field.setText(pass_field.getPromptText());
+            pass_field.setPromptText("Passcode");
+            pass_field.setDisable(isPasswordVisible);
+        }
+        // if(!pass_field.getText().isEmpty()){
+        // } else {
+        //     Timeline toggleTimeline = new Timeline(
+        //         new KeyFrame(Duration.seconds(0.5), e -> pass_field.setPromptText("Enter your passcode!")),
+        //         new KeyFrame(Duration.seconds(1.3), e -> pass_field.setPromptText("Passcode"))
+        //     );
+        //     toggleTimeline.setCycleCount(1);
+        //     toggleTimeline.play();
+        // }
+    }
+}
