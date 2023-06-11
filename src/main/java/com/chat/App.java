ccbe348d6b3314d35ebdf2bab4467aa3fb27302b
sgc93 <smachewgedefaw@gmail.com>
2023-06-10 17:25:04 -0900

    update implementation flow

--- src/main/java/com/chat/App.java
+++ src/main/java/com/chat/App.java
@@ -5,38 +5,23 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.chat.Model.Database;
-import com.chat.controller.HomeController;
 import com.chat.controller.LoginController;
 import com.chat.controller.NewAccountController;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         if(doesAccountExit()){
-            LoginController.stage = stage;
-            Parent root = FXMLLoader.load(App.class.getResource("Login.fxml"));
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.show();
+            LoginController loginController = new LoginController();
+            loginController.showLoginPage(primaryStage);
         } else {
-            NewAccountController.stage = stage;
-            Parent root = FXMLLoader.load(App.class.getResource("NewAccount.fxml"));
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.show();
-            // loginStage.setTitleBar();
+            NewAccountController newAccountController = new NewAccountController();
+            newAccountController.showNewAccountPage(primaryStage);
         }
-
     }
 
     private boolean doesAccountExit() {
@@ -44,11 +29,11 @@ public class App extends Application {
         try(Statement st = Database.connect().createStatement()) {
             ResultSet rs = st.executeQuery(sql);
             rs.next();
-            if(!rs.getString("username").isEmpty()){
+            if(! (rs.getString("username") == null)){
                 return true;
             }
         } catch (SQLException e) {
-            return false;
+            System.out.println("Database Error!");
         }
         return false;
     }
