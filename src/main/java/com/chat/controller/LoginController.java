bc01843579c7917f70e56eef52a7366c580672c7
sgc93 <smachewgedefaw@gmail.com>
2023-06-10 17:26:16 -0900

    make password field visibility changeable

--- src/main/java/com/chat/controller/LoginController.java
+++ src/main/java/com/chat/controller/LoginController.java
@@ -1,20 +1,28 @@
 package com.chat.controller;
 
+import java.io.IOException;
 import java.sql.*;
 
 import com.chat.Model.Database;
+import com.chat.utilities.Password;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class LoginController {
-    public static Stage stage;
-
     @FXML
     private Label error_label;
 
@@ -25,21 +33,25 @@ public class LoginController {
     private Button unlock_btn;
 
     @FXML
-    private TextField pass_field;
+    private SVGPath eye;
 
     @FXML
+    private PasswordField pass_field;
+    
+    @FXML
     private VBox root;
-
+    
     @FXML
     void closeLoginStage(ActionEvent event) {
         Stage stage = (Stage) unlock_btn.getScene().getWindow();
         stage.close();
     }
-
+    
     @FXML
     void unlock(ActionEvent event) throws Exception {
         VBox vbox = (VBox) unlock_btn.getParent();
-        TextField passfield = (TextField) vbox.getChildren().get(3);
+        HBox box = (HBox) vbox.getChildren().get(3);
+        TextField passfield = (TextField) box.getChildren().get(0);
         if(!isEmpty(passfield) && !isBlank(passfield) && isvalid(passfield)){
             Stage stage = (Stage) unlock_btn.getScene().getWindow();
             stage.close();
@@ -47,7 +59,7 @@ public class LoginController {
             homeController.showHomePage();
         }
     }
-
+    
     private boolean isEmpty(TextField passfield) {
         if(passfield.getText().isEmpty()){
             error_label.setText("Passcode field is Empty!");
@@ -63,7 +75,7 @@ public class LoginController {
         }
         return false;
     }
-
+    
     private boolean isvalid(TextField field) {
         String sql = "SELECT * FROM client";
         try(Statement st = Database.connect().createStatement()) {
@@ -72,12 +84,25 @@ public class LoginController {
             String password = rs.getString("password");
             if(field.getText().equals(password)){
                 return true;
-            }       
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         error_label.setText("Invalid Password!");
         return false;
     }
-}
+    
+    @FXML
+    void showPassword(ActionEvent event) {
+        Password.showPassword(pass_field, eye);
+    }
 
+    public void showLoginPage(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/chat/fxml/Login.fxml"));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.show();
+    }
+}
--- src/main/java/com/chat/controller/NewAccountController.java
+++ src/main/java/com/chat/controller/NewAccountController.java
@@ -1,37 +1,59 @@
 package com.chat.controller;
 
+import java.io.IOException;
+
 import com.chat.Model.Profile;
+import com.chat.utilities.Password;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class NewAccountController {
-
-    public static Stage stage;
-
+    private Stage stage;
     @FXML
     private Button close_btn;
 
     @FXML
-    private TextField cpw_field;
+    private PasswordField cpw_field;
 
     @FXML
     private Label error_label;
 
     @FXML
+    private SVGPath eye;
+
+    @FXML
     private TextField name_field;
+
+    @FXML
+    private PasswordField pass_field;
+
+    @FXML
+    private Button show_btn;
 
     @FXML
     private Button sign_btn;
 
-    @FXML
-    private TextField pw_field;
+    public NewAccountController() {
+    }
 
+    @FXML
+    void showPassword(ActionEvent event) {
+        Password.showPassword(pass_field, eye);
+    }
 
     @FXML
     void closeStage(ActionEvent event) throws Exception {
@@ -40,80 +62,75 @@ public class NewAccountController {
     
     @FXML
     void getIn(ActionEvent event) throws Exception {
-        VBox vbox = (VBox) sign_btn.getParent();
-        System.out.println(vbox);
-        if(!isEmpty(vbox) && isValid(vbox) && isStrong(vbox) && isPasswordConfirmed(vbox)){
+        if(!isEmpty() && isValid() && isLengthen() && isPasswordConfirmed()){
             Stage stage = (Stage) sign_btn.getScene().getWindow();
-            saveProfileData(vbox);
+            saveProfileData();
             stage.close();
             HomeController homeController = new HomeController(stage);
             homeController.showHomePage();
         } 
     }
     
-    private boolean isValid(VBox vbox) {
-        for(int j=0;j<3;j++){
-            TextField field = (TextField) vbox.getChildren().get(j);
-            for(int i=0;i < field.getText().length();i++){
-                if(field.getText().charAt(i) == '\'' || field.getText().charAt(i) == '"'){
-                    error_label.setText("*Using Quotation is not allowed!");
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private boolean isStrong(VBox vbox) {
-        TextField pass_field = (TextField) vbox.getChildren().get(1);
-        if(pass_field.getText().length() < 5){
-            error_label.setText("*Password length must be > 5");
+    private boolean isValid() {
+        if(name_field.getText().contains("\'") || name_field.getText().contains("\"") || pass_field.getText().contains("\'") || pass_field.getText().contains("\"" )){
+            error_label.setText("*Using Quotation is not allowed!");
             return false;
         }
         return true;
     }
 
-    private boolean isEmpty(VBox vbox) {
-        for(int j=0;j<3;j++){
-            TextField field = (TextField) vbox.getChildren().get(j);
-            if(field.getText().isBlank()) {
-                error_label.setText("*You have Empty Fields!");
-                return true;
-            } 
-            // int x = field.getText().length();
-            // for(int i=0;i < field.getText().length();i++){
-            //     if(field.getText().charAt(i) == ' '){
-            //         x = x - 1;
-            //     }
-            // }
-            // if(x == 0){
-            //     error_label.setText("*Using Only Spaces is not Valid Value!");
-            //     return true;
-            // }
+    private boolean isLengthen() {
+        if(pass_field.getText().length() < 5){
+            error_label.setText("*Password should have at least 5 characters.");
+            return false;
+        } else if(pass_field.getText().length() > 50){
+            error_label.setText("*Password length must be less than 50");
+        } else if(name_field.getText().length() > 20){
+            error_label.setText("*Username length must be less than 20");
+        }
+        return true;
+    }
+
+    private boolean isEmpty() {
+        if(name_field.getText().isBlank() || pass_field.getText().isBlank() || cpw_field.getText().isBlank()) {
+            error_label.setText("*You have Empty Fields!");
+            return true;
         }
         return false;
     }
 
-    private boolean isPasswordConfirmed(VBox vbox) {
-        TextField pass_field = (TextField) vbox.getChildren().get(1);
-        TextField con_field = (TextField) vbox.getChildren().get(2);
-        if(!pass_field.getText().equals(con_field.getText())){
+    private boolean isPasswordConfirmed() {
+        if(!pass_field.getText().equals(cpw_field.getText())){
             error_label.setText("Password is Uncormirmed!");
             return false;
         }
         return true;
     }
 
-    private void saveProfileData(VBox vbox) {
-        String username = ((TextField) vbox.getChildren().get(0)).getText();
-        String password = ((TextField) vbox.getChildren().get(1)).getText();
-        
-        Profile profile = new Profile(username, password);
+    private void saveProfileData() {
+        Profile profile = new Profile(name_field.getText(), pass_field.getText());
         profile.saveClientData();
     }
 
-    public void createNewAccount(ActionEvent event) throws Exception{
-        getIn(event);
+    public void showNewAccountPage(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/chat/fxml/NewAccount.fxml"));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.show();
+
+        initializeControls(root);
+    }
+
+    private void initializeControls(Parent root) {
+        VBox parent = (VBox) root;
+        HBox hbox = (HBox) parent.getChildren().get(1);
+        VBox vbox = (VBox) hbox.getChildren().get(2);
+        name_field = (TextField) vbox.getChildren().get(0);
+        HBox  box = (HBox) vbox.getChildren().get(1);
+        pass_field = (PasswordField) box.getChildren().get(0);
+        cpw_field = (PasswordField) vbox.getChildren().get(2);
+        name_field.requestFocus();
     }
 }
-
